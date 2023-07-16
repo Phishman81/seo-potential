6b96d0da6701b581697b74895207db45d8161231
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-07-16 07:26:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,26 +4,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-# The list of improvement scenarios
+# Die Liste der Verbesserungsszenarien
 scenarios = [
-    "Improve all rankings by 1 position",
-    "Improve all rankings by 2 positions",
-    "Improve all rankings by 5 positions",
-    "Improve all rankings by 10 positions",
-    "Improve all rankings by 20%",
-    "Improve all rankings by 50%",
-    "Improve all rankings by 100%",
-    "Lift all rankings below page 2 to random page 2 position",
-    "Lift all rankings below page 1 to random page 1 position",
-    "Lift all rankings to position 1"
+    "Verbessere alle Rankings um 1 Position",
+    "Verbessere alle Rankings um 2 Positionen",
+    "Verbessere alle Rankings um 5 Positionen",
+    "Verbessere alle Rankings um 10 Positionen",
+    "Verbessere alle Rankings um 20%",
+    "Verbessere alle Rankings um 50%",
+    "Verbessere alle Rankings um 100%",
+    "Hebe alle Rankings unter Seite 2 auf eine zufällige Position auf Seite 2 an",
+    "Hebe alle Rankings unter Seite 1 auf eine zufällige Position auf Seite 1 an",
+    "Hebe alle Rankings auf Position 1 an"
 ]
 
-# The list of CTR sources
-ctr_sources = ["Source 1", "Source 2", "Source 3"]
+# Die Liste der CTR-Quellen
+ctr_sources = ["Quelle 1", "Quelle 2", "Quelle 3"]
 
-# Function to get CTR by position based on source
+# Funktion um die CTR nach Position basierend auf der Quelle zu bekommen
 def get_ctr_by_position(ctr_source):
-    if ctr_source == "Source 1":
+    if ctr_source == "Quelle 1":
         return {
             1: 0.35,
             2: 0.25,
@@ -36,7 +36,7 @@ def get_ctr_by_position(ctr_source):
             9: 0.03,
             10: 0.02
         }
-    elif ctr_source == "Source 2":
+    elif ctr_source == "Quelle 2":
         return {
             1: 0.30,
             2: 0.24,
@@ -49,7 +49,7 @@ def get_ctr_by_position(ctr_source):
             9: 0.03,
             10: 0.02
         }
-    elif ctr_source == "Source 3":
+    elif ctr_source == "Quelle 3":
         return {
             1: 0.32,
             2: 0.26,
@@ -63,21 +63,21 @@ def get_ctr_by_position(ctr_source):
             10: 0.01
         }
 
-# Calculate potential traffic based on scenario
+# Berechne den potenziellen Traffic basierend auf dem Szenario
 def calculate_potential_traffic_based_on_scenario(data, scenario, avg_ctr_by_position):
-    if "Improve all rankings by" in scenario:
-        if "position" in scenario:
-            improvement = int(scenario.split()[4])  # extract the number of positions to improve
+    if "Verbessere alle Rankings um" in scenario:
+        if "Position" in scenario:
+            improvement = int(scenario.split()[4])  # extrahiere die Anzahl der zu verbessernden Positionen
             data['Adjusted Ranking Position'] = data['Current Ranking Position'].apply(lambda x: max(x - improvement, 1))
         elif "%" in scenario:
-            improvement = int(scenario.split()[4].strip('%')) / 100  # extract the percentage to improve
+            improvement = int(scenario.split()[4].strip('%')) / 100  # extrahiere den Prozentsatz der Verbesserung
             data['Adjusted Ranking Position'] = data['Current Ranking Position'].apply(lambda x: max(x * (1 - improvement), 1))
-    elif "Lift all rankings below" in scenario:
-        target_page = int(scenario.split()[4])  # extract the target page
-        lower_bound = 10 * (target_page - 1) + 1  # calculate the lower bound of the target page
-        upper_bound = 10 * target_page  # calculate the upper bound of the target page
+    elif "Hebe alle Rankings unter" in scenario:
+        target_page = int(scenario.split()[4])  # extrahiere die Ziel-Seite
+        lower_bound = 10 * (target_page - 1) + 1  # berechne die untere Grenze der Ziel-Seite
+        upper_bound = 10 * target_page  # berechne die obere Grenze der Ziel-Seite
         data['Adjusted Ranking Position'] = data['Current Ranking Position'].apply(lambda x: random.randint(lower_bound, upper_bound) if x > upper_bound else x)
-    elif scenario == "Lift all rankings to position 1":
+    elif scenario == "Hebe alle Rankings auf Position 1 an":
         data['Adjusted Ranking Position'] = 1
     data['Potential CTR'] = data['Adjusted Ranking Position'].apply(lambda x: avg_ctr_by_position[min(round(x), 10)])
     data['Potential Traffic'] = data['Potential CTR'] * data['Monthly Search Volume per Keyword']
@@ -85,55 +85,79 @@ def calculate_potential_traffic_based_on_scenario(data, scenario, avg_ctr_by_pos
 
 def main():
     st.title('SEO Potential Analyzer')
-    st.write('Please upload your CSV file below. The file should contain the following columns: Keyword, Keyword Cluster, Monthly Search Volume per Keyword, Current Clicks per Month for this Website, Current Ranking Position, Current CTR per Keyword for the Website.')
+    st.write('Bitte laden Sie Ihre CSV-Datei hoch. Die Datei sollte die folgenden Spalten enthalten: Keyword, Keyword Cluster, monatliches Suchvolumen pro Keyword, aktuelle Klicks pro Monat für diese Website, aktuelle Ranking-Position, aktuelle CTR pro Keyword für die Website.')
 
-    uploaded_file = st.file_uploader("Choose a CSV file", type='csv')
+    uploaded_file = st.file_uploader("Wählen Sie eine CSV-Datei aus", type='csv')
 
     if uploaded_file is not None:
         data = pd.read_csv(uploaded_file)
 
         scenario = st.selectbox(
-            'Select an improvement scenario',
+            'Wählen Sie ein Verbesserungsszenario aus',
             scenarios
         )
 
         ctr_source = st.selectbox(
-            'Select a CTR source',
+            'Wählen Sie eine CTR-Quelle aus',
             ctr_sources
         )
 
         avg_ctr_by_position = get_ctr_by_position(ctr_source)
-        
+
         data = calculate_potential_traffic_based_on_scenario(data, scenario, avg_ctr_by_position)
 
         st.dataframe(data)
-        
-        st.subheader('Total Current Traffic')
-        st.write(data['Current Clicks per Month for this Website'].sum())
-        
-        st.subheader('Total Potential Traffic')
-        st.write(data['Potential Traffic'].sum())
+
+        st.subheader('Gesamtaktuelle Traffic')
+        current_traffic = data['Current Clicks per Month for this Website'].sum()
+        st.write(current_traffic)
+
+        st.subheader('Gesamtpotenzial Traffic')
+        potential_traffic = data['Potential Traffic'].sum()
+        st.write(potential_traffic)
 
         fig, ax = plt.subplots()
 
-        index = np.arange(len(data))
         bar_width = 0.35
 
-        opacity = 0.8
-        rects1 = plt.bar(index, data['Current Clicks per Month for this Website'], bar_width,
-        alpha=opacity,
+        rects1 = plt.bar(1, current_traffic, bar_width,
         color='b',
-        label='Current Traffic')
+        label='Gesamtaktuelle Traffic')
 
-        rects2 = plt.bar(index + bar_width, data['Potential Traffic'], bar_width,
-        alpha=opacity,
+        rects2 = plt.bar(1 + bar_width, potential_traffic, bar_width,
         color='g',
-        label='Potential Traffic')
+        label='Gesamtpotenzial Traffic')
 
-        plt.xlabel('Keywords')
+        plt.xlabel('Traffic-Typ')
         plt.ylabel('Traffic')
-        plt.title('Current vs Potential Traffic')
-        plt.xticks(index + bar_width, data['Keyword'], rotation=90)
+        plt.title('Gesamtaktuelle Traffic vs. Gesamtpotenzial Traffic')
+        plt.xticks([1, 1 + bar_width], ['Gesamtaktuelle Traffic', 'Gesamtpotenzial Traffic'])
+        plt.legend()
+
+        plt.tight_layout()
+        st.pyplot(fig)
+
+        # Erstelle ein Balkendiagramm für die Keyword Cluster
+        fig, ax = plt.subplots()
+
+        keyword_clusters = data['Keyword Cluster'].unique()
+        index = np.arange(len(keyword_clusters))
+        
+        current_traffic_per_cluster = [data[data['Keyword Cluster'] == cluster]['Current Clicks per Month for this Website'].sum() for cluster in keyword_clusters]
+        potential_traffic_per_cluster = [data[data['Keyword Cluster'] == cluster]['Potential Traffic'].sum() for cluster in keyword_clusters]
+
+        rects1 = plt.bar(index, current_traffic_per_cluster, bar_width,
+        color='b',
+        label='Gesamtaktuelle Traffic')
+
+        rects2 = plt.bar(index + bar_width, potential_traffic_per_cluster, bar_width,
+        color='g',
+        label='Gesamtpotenzial Traffic')
+
+        plt.xlabel('Keyword Cluster')
+        plt.ylabel('Traffic')
+        plt.title('Gesamtaktuelle Traffic vs. Gesamtpotenzial Traffic pro Keyword Cluster')
+        plt.xticks(index + bar_width, keyword_clusters, rotation=90)
         plt.legend()
 
         plt.tight_layout()
