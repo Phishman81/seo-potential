0a1c94fad078f2fb6f9c9dcc3ce1d80564b684d1
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-07-25 00:16:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
+
 import pandas as pd
 import streamlit as st
 import numpy as np
 import random
 
 # Customize the layout
-st.set_page_config(page_title="SEO-Potential-Analysis", page_icon="🤖", layout="wide", )
-st.markdown(f"""
+st.set_page_config(page_title="SEO-Potential-Analysis", page_icon="🤖", layout="wide", )     
+st.markdown(f'''
             <style>
-            .stApp {{background-image: url("https://images.unsplash.com/photo-1509537257950-20f875b03669?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80");
+            .stApp {{background-image: url("https://images.unsplash.com/photo-1509537257950-20f875b03669?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80"); 
                      background-attachment: fixed;
                      background-size: cover}}
          </style>
-         """, unsafe_allow_html=True)
+         ''', unsafe_allow_html=True)
 
 # The list of improvement scenarios
 scenarios = [
-    "Improve rankings by X positions",
-    "Improve all rankings by 10%",
+    "Improve rankings by X positions", 
+    "Improve all rankings by 10%", 
     "Lift all to random page 2 position",
     "Lift all to random page 1 position",
     "Lift all to position 1"
@@ -38,21 +39,32 @@ def get_ctr_by_position():
     }
 
 # Calculate potential traffic based on scenario
-def calculate_potential_traffic_based_on_scenario(data, scenario, avg_ctr_by_position):
-    if scenario == scenarios[0]:  # Improve rankings by X positions
-        data['Adjusted Ranking Position'] = data['Current Ranking Position'].apply(lambda x: x - 1 if x > 1 else x)
-    elif scenario == scenarios[1]:  # Improve all rankings by 10%
-        data['Adjusted Ranking Position'] = data['Current Ranking Position'].apply(lambda x: round(x * 0.9) if x > 1 else x)
-    elif scenario == scenarios[2]:  # Lift all to random page 2 position
-        data['Adjusted Ranking Position'] = data['Current Ranking Position'].apply(lambda x: random.randint(11, 20) if x > 20 else x)
-    elif scenario == scenarios[3]:  # Lift all to random page 1 position
-        data['Adjusted Ranking Position'] = data['Current Ranking Position'].apply(lambda x: random.randint(1, 10) if x > 10 else x)
-    elif scenario == scenarios[4]:  # Lift all to position 1
-        data['Adjusted Ranking Position'] = 1
+def calculate_potential_traffic_based_on_scenario(data, scenario, avg_ctr_by_position, monthly_rates):
+    monthly_projections = pd.DataFrame()
 
-    data['Potential CTR'] = data['Adjusted Ranking Position'].apply(lambda x: avg_ctr_by_position.get(min(round(x), 10), 0))
-    data['Potential Traffic'] = data['Potential CTR'] * data['Monthly Search Volume per Keyword']
-    return data
+    for month, rate in zip(range(1, 25), monthly_rates):  # 24 months
+        if scenario == scenarios[0]: # Improve rankings by X positions
+            data['Adjusted Ranking Position'] = data['Current Ranking Position'].apply(lambda x: x - rate if x > 1 else x)
+        elif scenario == scenarios[1]: # Improve all rankings by 10%
+            data['Adjusted Ranking Position'] = data['Current Ranking Position'].apply(lambda x: x - 0.1 * rate if x > 1 else x)
+        elif scenario == scenarios[2]: # Lift all to random page 2 position
+            data['Adjusted Ranking Position'] = data['Current Ranking Position'].apply(lambda x: random.randint(11, 20) if x > 20 else x)
+        elif scenario == scenarios[3]: # Lift all to random page 1 position
+            data['Adjusted Ranking Position'] = data['Current Ranking Position'].apply(lambda x: random.randint(1, 10) if x > 10 else x)
+        elif scenario == scenarios[4]: # Lift all to position 1
+            data['Adjusted Ranking Position'] = 1
+
+        data['Potential CTR'] = data['Adjusted Ranking Position'].apply(lambda x: avg_ctr_by_position.get(min(round(x), 10), 0))
+        data['Potential Traffic'] = data['Potential CTR'] * data['Monthly Search Volume per Keyword']
+        
+        new_monthly_projection = pd.DataFrame({
+            'Month': [month],
+            'Total Potential Traffic': [data['Potential Traffic'].sum()]
+        })
+        
+        monthly_projections = monthly_projections.append(new_monthly_projection, ignore_index=True)
+
+    return monthly_projections
 
 def main():
     st.title('SEO Potential Analyzer')
@@ -65,14 +77,14 @@ def main():
             data = pd.read_csv(uploaded_file)
 
             expected_columns = [
-                "Keyword",
-                "Keyword Cluster",
-                "Monthly Search Volume per Keyword",
-                "Current Clicks per Month for this Website",
-                "Current Ranking Position",
+                "Keyword", 
+                "Keyword Cluster", 
+                "Monthly Search Volume per Keyword", 
+                "Current Clicks per Month for this Website", 
+                "Current Ranking Position", 
                 "Current CTR per Keyword for the Website"
             ]
-
+            
             if not all(col in data.columns for col in expected_columns):
                 st.error("CSV file must contain the following columns: " + ', '.join(expected_columns))
                 return
@@ -80,12 +92,6 @@ def main():
             scenario = st.selectbox('Select an improvement scenario', scenarios)
 
             avg_ctr_by_position = get_ctr_by_position()
-            data = calculate_potential_traffic_based_on_scenario(data, scenario, avg_ctr_by_position)
-
-            st.dataframe(data)
-
-            current_traffic = data['Current Clicks per Month for this Website'].sum()
-            potential_traffic = data['Potential Traffic'].sum()
 
             # Define manual monthly improvement rates over 24 months
             monthly_rates = [
@@ -95,18 +101,12 @@ def main():
                 0.65, 0.70, 0.75, 0.80, 0.85, 1.00
             ]
 
-            monthly_traffics = [current_traffic]
-            for rate in monthly_rates:
-                monthly_traffics.append(monthly_traffics[-1] + (potential_traffic - current_traffic) * rate)
+            monthly_projections = calculate_potential_traffic_based_on_scenario(data, scenario, avg_ctr_by_position, monthly_rates)
 
-            st.subheader('Improvement Trajectory')
-            st.line_chart(pd.DataFrame({
-                'Month': list(range(25)),
-                'Traffic': monthly_traffics
-            }))
+            st.dataframe(monthly_projections)
 
         except Exception as e:
             st.error(f'Error reading file: {e}')
-
+        
 if __name__ == "__main__":
     main()
